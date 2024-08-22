e589bb9bf7e270231639278d2b39adc165188c07
mpabarca <maria@shape.dk>
2024-08-22 20:03:33 +0200

    Delete unnecessary console.log

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -3,12 +3,7 @@ import { LanguageCode, Namespaces } from './enum';
 
 const importLocale = async (locale: LanguageCode, namespace: Namespaces) => {
   try {
-    console.log('importLocale - locale', locale)
-    console.log('importLocale - namespace', namespace)
-
     const module = await import(`./${locale}/${namespace}`)
-    console.log('importLocale - typeof module', typeof module)
-    console.log('importLocale - module', module[namespace])
 
     return module[namespace]
   } catch (error) {
@@ -21,10 +16,6 @@ export const getTranslation = async (
   locale: LanguageCode,
   namespace: Namespaces
 ) => {
-  console.log('getTranslation - locale', locale)
-  console.log('getTranslation - namespace', namespace)
-
   const translation = await importLocale(locale, namespace);
-  console.log('getTranslation - translation', translation)
   return translation;
 };
